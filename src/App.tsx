63c684a12e616f86a2c4952a2cab6fafe5752800
凌莞 <i@gao4.pw>
2022-01-17 13:36:43 +0800

    引入 vite 环境变量

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import DateGroup from './types/DateGroup'
 import DateContainer from './components/DateContainer'
 import processHistory from './utils/processHistory'
 
-const BASE_URL = 'http://127.0.0.1:5000/api'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000/api'
 
 function App() {
     const [data, setData] = useState<DateGroup[] | undefined>(undefined)
