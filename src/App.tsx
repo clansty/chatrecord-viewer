d765b1735322b3b11183a7e4eeec6a05f37c8e8c
Clansty <i@gao4.pw>
2023-11-10 22:07:09 +0800

    支持 tgWebAppStartParam

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,42 @@ import processHistory from './utils/processHistory'
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000/api'
 
 function App() {
-    const [data, setData] = useState<DateGroup[] | undefined>(undefined)
-    const [error, setError] = useState(false)
+  const [data, setData] = useState<DateGroup[] | undefined>(undefined)
+  const [error, setError] = useState(false)
 
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search)
-        let fetchUrl = ''
-        if (params.get('res') && params.get('sign')) {
-            fetchUrl = `${BASE_URL}/records${window.location.search}`
-        }
-        else if (params.get('hash')) {
-            fetchUrl = `/api/get/${params.get('hash')}`
-        }
-        if (!fetchUrl) {
-            setError(true)
-        }
-        fetch(fetchUrl)
-            .then(res => res.json())
-            .then(data => setData(processHistory(data as any)))
-            .catch(e => setError(e))
-    }, [])
-
-    if (error) {
-        console.log(error)
-        return <div>Error</div>
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search)
+    let fetchUrl = ''
+    if (params.get('res') && params.get('sign')) {
+      fetchUrl = `${BASE_URL}/records${window.location.search}`
     }
+    else if (params.get('hash') || params.get('tgWebAppStartParam')) {
+      fetchUrl = `/api/get/${params.get('hash') || params.get('tgWebAppStartParam')}`
+    }
+    if (!fetchUrl) {
+      setError(true)
+    }
+    fetch(fetchUrl)
+      .then(res => res.json())
+      .then(data => setData(processHistory(data as any)))
+      .catch(e => setError(e))
+  }, [])
 
-    if (!data)
-        return <div>Loading...</div>
+  if (error) {
+    console.log(error)
+    return <div>:(</div>
+  }
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.container}>
-                {data.map(e => <DateContainer group={e} key={e.date}/>)}
-            </div>
-        </div>
-    )
+  if (!data)
+    return <div>Loading...</div>
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.container}>
+        {data.map(e => <DateContainer group={e} key={e.date}/>)}
+      </div>
+    </div>
+  )
 }
 
 export default App
