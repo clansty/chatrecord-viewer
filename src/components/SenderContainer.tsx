9b7f3f0fcb2be80716a20b9e7df77ee3ef0293af
Clansty <i@gao4.pw>
2023-11-10 17:18:46 +0800

    头像出错时设置为未知头像

--- src/components/SenderContainer.tsx
+++ src/components/SenderContainer.tsx
@@ -3,15 +3,18 @@ import SenderGroup from '../types/SenderGroup'
 import SenderNameBubble from './SenderNameBubble'
 import MessageBubble from './MessageBubble'
 import noAvatar from '../assets/no-avatar.webp'
+import {useState} from 'react'
 
 export default function SenderContainer({group}: { group: SenderGroup }) {
+  const [avatar, setAvatar] = useState(group.avatar)
+
   return <div className={styles.container}>
     <div className={styles.avatarContainer}>
       <img
-        src={group.avatar}
+        src={avatar}
         alt={`头像：${group.senderId}`}
         referrerPolicy="no-referrer"
-        onError={() => group.avatar = noAvatar}
+        onError={() => setAvatar(noAvatar)}
       />
     </div>
     <div className={styles.mainContainer}>
